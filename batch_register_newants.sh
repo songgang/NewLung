91f3d3e3b7afb6d35f0068669313ab61d0df1a35
Gang Song <songgang@picsl.upenn.edu>
2014-05-05 19:36:20 -0400

    add new ants for registration

---
+++ batch_register_newants.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# batch script to register EduardoNewData 
+# fix: insp
+# mov: exp
+
+make_metric_dir(){
+
+local RESDIR=$1
+
+    if [ -f $RESDIR ]
+    then
+        echo 'As a file: ' $RESDIR
+        rm $RESDIR
+    fi
+
+    if [ ! -d $RESDIR ]
+    then
+        echo creating $RESDIR
+        mkdir $RESDIR
+    fi
+}
+
+
+func(){
+for mydate in $datelist
+do
+    echo process: $dbroot/$mydate    
+
+    d$mydate
+    local let nb_img=`wc -w <<< $imglist`
+    echo total $nb_img files
+    local i
+    for((i=1; i <= nb_img; i+=2 ))
+    do
+        local fixname
+        local movname
+        local j
+        let j=$i+1
+
+        fixname=`awk "{print $"$i"}" <<< $imglist`
+        movname=`awk "{print $"$j"}" <<< $imglist`
+
+        echo ===========================================================
+        echo image fix mov: $fixname $movname
+
+        FIXLUNGNAME=$fixname'_downsampled'
+        FIXLUNGIMG=$RESROOT/$fixname/$FIXLUNGNAME'.nii.gz'
+        FIXLUNGMASK=$RESROOT/$fixname/$FIXLUNGNAME'_lungmask.nii.gz'
+
+        MOVLUNGNAME=$movname'_downsampled'
+        MOVLUNGIMG=$RESROOT/$movname/$MOVLUNGNAME'.nii.gz'
+        MOVLUNGMASK=$RESROOT/$movname/$MOVLUNGNAME'_lungmask.nii.gz'
+
+        RESDIR=$RESROOT/$fixname
+        
+        if [ ! -f $FIXLUNGIMG ]; then echo $FIXLUNGIMG does not exist!; continue; fi
+        if [ ! -f $MOVLUNGIMG ]; then echo $MOVLUNGIMG does not exist!; continue; fi
+        
+        make_metric_dir $RESDIR
+
+        #old ants, 1mm resolution 32g * 25%=8g needs -pe serial 4
+        qsub -pe serial 4 -S /bin/bash -N ${fixname}_newants_register -wd $RESDIR register_newants.sh $FIXLUNGIMG $FIXLUNGMASK $MOVLUNGIMG $MOVLUNGMASK $RESDIR
+
+    done
+done
+}
+
+# to obtain $dbroot $mydate $myimg 
+. dblist.sh
+
+RESROOT=/home/songgang/project/EduardoNewData/data/output
+CURDIR=`pwd`
+echo $datelist
+func
---
+++ register_newants.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+##
+# This script performs the following operations:
+#   1. registers (using ANTS) $1-fixed to $2-moving
+# 
+##
+
+if [[ $NSLOTS -gt 1 ]]; then
+  ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=$NSLOTS
+else
+  ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=1
+fi
+
+tic(){
+START=$(date +%s)
+}
+
+toc(){
+# START=$(date +%s)
+END=$(date +%s)
+DIFF=$(( $END - $START ))
+echo "It took $DIFF seconds"
+}
+
+function register_new_ants()
+{
+  dim=3 # image dimensionality
+  AP=/home/songgang/project/ANTS/gccrel/bin/  # path to ANTs binaries
+
+  casename=$1
+  f=$2; fmask=$3; m=$4; mmask=$5; # fixed and moving image file names 
+  OUTPUTDIR=$6;
+  
+  if [[ ! -s $f ]] ; then echo no fixed $f ; exit; fi
+  if [[ ! -s $m ]] ; then echo no moving $m ;exit; fi
+  
+  nm1=` basename $f | cut -d '.' -f 1 `
+  nm2=` basename $m | cut -d '.' -f 1 `
+
+  reg=${AP}/antsRegistration
+  
+  
+  nm=$OUTPUTDIR/${nm1}_fixed_${nm2}_moving_${casename}   # construct output prefix
+
+  echo register fix $f 
+  echo register mov $m 
+  echo outname is $nm 
+
+
+  its=10000x10000x10000x10000x10000
+  percentage=0.1
+
+  $reg -d $dim -r [ $f, $m ,1]  \
+    -m mattes[  $fmask, $mmask, 1 , 32, regular, $percentage ] \
+     -t translation[ 0.1 ] \
+     -c [$its,1.e-8,20]  \
+    -s 6x4x3x2x2vox  \
+    -f 12x8x6x4x2 -l 1 \
+    -m mattes[  $fmask, $mmask, 1 , 32, regular, $percentage ] \
+     -t rigid[ 0.1 ] \
+     -c [$its,1.e-8,20]  \
+    -s 4x3x2x2x2vox  \
+    -f 8x6x4x2x1 -l 1 \
+    -m mattes[  $fmask, $mmask, 1 , 32, regular, $percentage ] \
+     -t affine[ 0.1 ] \
+     -c [$its,1.e-8,20]  \
+    -s 4x3x2x2x2vox  \
+    -f 8x6x4x2x1 -l 1 \
+    -m CC[  $f, $m , 1 , 2 ] \
+     -t SyN[ .25, 6, 0 ] \
+     -c [ 200x200x200x200x200x50,0,5 ]  \
+    -s 6x4x3x2x1x1vox  \
+    -f 12x8x6x4x2x1 -l 1 -u 1 -z 1 \
+   -o ${nm}
+
+  ${AP}antsApplyTransforms -d $dim -i $m -r $f -n linear -t ${nm}1Warp.nii.gz -t ${nm}0GenericAffine.mat -o ${nm}_warped.nii.gz
+}
+
+tic
+
+FIX=$1
+FIXMASK=$2
+MOV=$3
+MOVMASK=$4
+OUTPUTDIR=$5
+
+register_new_ants newants $FIX $FIXMASK $MOV $MOVMASK $OUTPUTDIR 
+
+toc
