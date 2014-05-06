996ab2495da33b65a1f4dfc38e365aac56536d57
Gang Song <songgang@picsl.upenn.edu>
2014-05-06 13:36:01 -0400

    test register new ants for 1.5mm resolution

--- batch_register_newants.sh
+++ batch_register_newants.sh
@@ -44,11 +44,13 @@ do
         echo ===========================================================
         echo image fix mov: $fixname $movname
 
-        FIXLUNGNAME=$fixname'_downsampled'
+        #FIXLUNGNAME=$fixname'_downsampled'
+        FIXLUNGNAME=$fixname'_down1p5mm'
         FIXLUNGIMG=$RESROOT/$fixname/$FIXLUNGNAME'.nii.gz'
         FIXLUNGMASK=$RESROOT/$fixname/$FIXLUNGNAME'_lungmask.nii.gz'
 
-        MOVLUNGNAME=$movname'_downsampled'
+        #MOVLUNGNAME=$movname'_downsampled'
+        MOVLUNGNAME=$movname'_down1p5mm'
         MOVLUNGIMG=$RESROOT/$movname/$MOVLUNGNAME'.nii.gz'
         MOVLUNGMASK=$RESROOT/$movname/$MOVLUNGNAME'_lungmask.nii.gz'
 
@@ -61,7 +63,7 @@ do
 
         #old ants, 1mm resolution 32g * 25%=8g needs -pe serial 4
         #new ants, serial 4 not enough, increase to 7
-        qsub -pe serial 7 -S /bin/bash -N ${fixname}_newants_register -wd $RESDIR register_newants.sh $FIXLUNGIMG $FIXLUNGMASK $MOVLUNGIMG $MOVLUNGMASK $RESDIR
+        echo qsub -pe serial 1 -S /bin/bash -N ${FIXLUNGNAME}_newants_register -wd $RESDIR register_newants.sh $FIXLUNGIMG $FIXLUNGMASK $MOVLUNGIMG $MOVLUNGMASK $RESDIR
 
     done
 done
