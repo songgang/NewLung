0d14dcb2a9c7701d6ad2c8cff4486df27492cbca
Gang Song <songgang@picsl.upenn.edu>
2014-05-08 14:10:09 -0400

    debug copy header information to all segmentation masks as Nick's Utilities not copying orientation

--- seglungc.sh
+++ seglungc.sh
@@ -28,11 +28,25 @@ TMPIMAGE=$INPUTIMAGE
 
 UTILITIESDIR=/home/songgang/project/tustison/Utilities/gccrel
 #UTILITIESDIR=/home/songgang/project/tustison/Utilities/gccrel_before_May_04_2014
+ANTSDIR=/home/songgang/project/ANTS/gccrel/bin
 
-MYDO ${UTILITIESDIR}/ExtractLungs $TMPIMAGE $LUNGMASK 
-MYDO ${UTILITIESDIR}/SegmentAirways $TMPIMAGE $LUNGMASK $AIRWAYMASK
-MYDO ${UTILITIESDIR}/SeparateLungs $TMPIMAGE $AIRWAYMASK $SEPARATEMASK
-MYDO ${UTILITIESDIR}/SmoothLungs $SEPARATEMASK $SMOOTHMASK 15
-c3d $SMOOTHMASK -threshold 2 3 1 0 -o $RESDIR/${LUNGNAME}_lungmask.nii.gz
+# MYDO ${UTILITIESDIR}/ExtractLungs $TMPIMAGE $LUNGMASK 
+# MYDO ${UTILITIESDIR}/SegmentAirways $TMPIMAGE $LUNGMASK $AIRWAYMASK
+# MYDO ${UTILITIESDIR}/SeparateLungs $TMPIMAGE $AIRWAYMASK $SEPARATEMASK
+# MYDO ${UTILITIESDIR}/SmoothLungs $SEPARATEMASK $SMOOTHMASK 15
+# c3d $SMOOTHMASK -threshold 2 3 1 0 -o $RESDIR/${LUNGNAME}_lungmask.nii.gz
+
+
+# copy header information as it seems that
+# Nick's segmentation did not copy orientation
+$ANTSDIR/CopyImageHeaderInformation $INPUTIMAGE $LUNGMASK $LUNGMASK 1 1 1
+$ANTSDIR/CopyImageHeaderInformation $INPUTIMAGE $AIRWAYMASK $AIRWAYMASK 1 1 1
+$ANTSDIR/CopyImageHeaderInformation $INPUTIMAGE $SEPARATEMASK $SEPARATEMASK 1 1 1
+$ANTSDIR/CopyImageHeaderInformation $INPUTIMAGE $SMOOTHMASK $SMOOTHMASK 1 1 1
+$ANTSDIR/CopyImageHeaderInformation $INPUTIMAGE $RESDIR/${LUNGNAME}_lungmask.nii.gz $RESDIR/${LUNGNAME}_lungmask.nii.gz 1 1 1
+
+
+
+
 
 
