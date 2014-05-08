b91e1ff250c23b8e01eb02e4e1a18209c29e7caf
Gang Song <songgang@picsl.upenn.edu>
2014-05-08 14:11:42 -0400

    debug do same copy to ***_downsampled

--- batch_seglungc.sh
+++ batch_seglungc.sh
@@ -43,9 +43,9 @@ do
         
         # LUNGIMG=$dbroot/$mydate/$myimg/$myimg'.nii.gz'
 
-        # LUNGNAME=$myimg'_downsampled'
+        LUNGNAME=$myimg'_downsampled'
         # LUNGIMG=$dbroot/$mydate/$myimg/$myimg'_downsampled.nii.gz'       
-        LUNGNAME=$myimg'_down1p5mm'
+        # LUNGNAME=$myimg'_down1p5mm'
 
         LUNGIMG=/home/songgang/project/EduardoNewData/data/output/$myimg/$LUNGNAME'.nii.gz'       
         RESDIR=$RESROOT/$myimg
