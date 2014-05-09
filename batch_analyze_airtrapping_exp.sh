0efa609babf71beab6bc594e313f977b1b6c6340
Gang Song <songgang@picsl.upenn.edu>
2014-05-09 15:20:36 -0400

    debug batch_analyze_airtrapping_exp.sh

--- batch_analyze_airtrapping_exp.sh
+++ batch_analyze_airtrapping_exp.sh
@@ -53,24 +53,19 @@ do
 
         #FIXLUNGNAME=$fixname'_down1p5mm'
         FIXLUNGNAME=$fixname'_downsampled'
-        FIXLUNGIMG=$RESROOT/$fixname/$FIXLUNGNAME'.nii.gz'
-        FIXLUNGMASK=$RESROOT/$fixname/$FIXLUNGNAME'_lungmask.nii.gz'
 
         #MOVLUNGNAME=$movname'_down1p5mm'
         MOVLUNGNAME=$movname'_downsampled'
-        MOVLUNGIMG=$RESROOT/$movname/$MOVLUNGNAME'.nii.gz'
-        MOVLUNGMASK=$RESROOT/$movname/$MOVLUNGNAME'_lungmask.nii.gz'
-
-        RESDIR=$RESROOT/$fixname
         
+        RESDIR=$RESROOT/$fixname
+        casename="oldants"
+
         if [ ! -f $FIXLUNGIMG ]; then echo $FIXLUNGIMG does not exist!; continue; fi
         if [ ! -f $MOVLUNGIMG ]; then echo $MOVLUNGIMG does not exist!; continue; fi
         
         make_metric_dir $RESDIR
 
-        #old ants, 1mm resolution 32g * 25%=8g needs -pe serial 4
-        # 1.5mm resolution try 4G
-        qsub -pe serial 4 -S /bin/bash -N ${FIXLUNGNAME}_oldants -wd $RESDIR register.sh $FIXLUNGIMG $FIXLUNGMASK $MOVLUNGIMG $MOVLUNGMASK $RESDIR
+        echo qsub -pe serial 1 -S /bin/bash -j y -N ${FIXLUNGNAME}_airtrapping -wd $RESDIR analyze_airtrapping_exp.sh $FIXLUNGIMG $MOVLUNGIMG $RESDIR $casename
 
     done
 done
