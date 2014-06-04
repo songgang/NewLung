4759dd29418257183bd5a120fff8939c91689893
Gang Song <songgang@seas.upenn.edu>
2014-06-04 11:58:31 -0400

    remove the old directory. remember this would affect directories during converting dicom to nii

--- move_nii.sh
+++ move_nii.sh
@@ -4,3 +4,18 @@ INPUTDIR=/home/songgang/project/EduardoNewData/data/input/nii
 TMPOUTPUTDIR=/home/songgang/project/EduardoNewData/data/input/nii2
 
 for a in `ls $INPUTDIR`; do mv $INPUTDIR/$a/* $TMPOUTPUTDIR; done
+echo rm -rf $INPUTDIR
+echo mv $TMPOUTPUTDIR $INPUTDIR
+
+# for a in `ls $INPUTDIR`; do
+# 	for b in `ls $INPUTDIR/$a`; do
+# 		for c in `ls $INPUTDIR/$a/$b`; do
+# 			if [ -f $INPUTDIR/$a/$b/$c ]; then
+# 				ls -l $INPUTDIR/$a/$b/$c
+# 				ls -l $TMPOUTPUTDIR/$b/$c
+# 				rm $TMPOUTPUTDIR/$b/$c
+# 				mv $INPUTDIR/$a/$b/$c $TMPOUTPUTDIR/$b
+# 			fi
+# 		done
+# 	done
+# done
